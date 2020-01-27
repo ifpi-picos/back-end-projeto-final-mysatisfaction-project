110319ffc07e7e1e7fe5a422b90dfe1ad43009a0
jackson <lealjackson@hotmail.com>
2020-01-27 09:20:20 -0300

    Links

--- server.js
+++
@@ -1,9 +0,0 @@
-import setupApp from './src/app';
-const port = 3000;
-
-setupApp()
-  .then(app => app.listen(process.env.PORT || port, () => console.log(`Aplicativo rodando na PORT: ${PORT}`)))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
--- src/models/welcome.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const schema = new mongoose.Schema({
-  name: String,
-});
-const Welcome = mongoose.model('Welcome', schema);
-
-export default Welcome;
